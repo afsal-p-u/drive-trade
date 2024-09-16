277b73abac31f6c47dc35c86e3aab6c31427286b
afsal p u <afsalpu07@gmail.com>
2024-09-16 22:14:47 +0530

    changes

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,6 +3,5 @@ const { SignUp, SignIn } = require('../controllers/user.controller')
 
 router.post('/sign-up', SignUp)
 router.post('/sign-in', SignIn)
-router.post('/sign-out', () => {})
 
 module.exports = router
